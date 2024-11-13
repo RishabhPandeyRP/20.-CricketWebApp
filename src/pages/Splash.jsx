f5bdb1af85bc7700d37935802328e74e26f9d8f7
RishabhPandeyRP <rishabh17704@gmail.com>
2024-11-13 22:31:38 +0530

    splash screen

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -1,30 +1,30 @@
-import splashImg from "../assets/Rectangle 1.svg"
-import { useNavigate } from "react-router-dom"
+import splashImg from "../assets/Rectangle 1.svg";
+import { useNavigate } from "react-router-dom";
 
 const Splash = () => {
-
     const navigate = useNavigate();
 
     return (
-        <div className="w-full h-fit bg-[#050F40] flex flex-col justify-center items-center gap-3 py-4">
-            <div className="-mt-3 border-white">
-                <img src={splashImg} alt="" className=" border-white w-[590px] h-[590px] ml-5" />
+        <div className="w-full h-[100vh] bg-[#050F40] flex flex-col justify-center items-center gap-3 py-4  border-red-500 overflow-hidden">
+            <div className="-mt-3  border-white">
+                <img src={splashImg} alt="" className="border-white w-[90vw] max-w-[590px] h-auto" />
             </div>
-            <div className="flex flex-col gap-3  border-white justify-center items-center">
-                <div className="font-bold text-[26px] text-center text-white w-[88%]">
-                    Discover the Thrill of
-                    Cricket Auction
+            <div className="flex flex-col gap-3  border-green-700 justify-center items-center w-full px-4 h-[30%]">
+                <div className="font-bold text-[5vw] md:text-[26px] text-center text-white w-full">
+                    Discover the Thrill of Cricket Auction
                 </div>
-                <div className="text-[13px] text-center text-white w-[85%]">
-                    Experience Cricket Auctions, auction updates, and exclusive
-                    content from the Aucto Gaming.
+                <div className="text-[3vw] md:text-[13px] text-center text-white w-full">
+                    Experience Cricket Auctions, auction updates, and exclusive content from Aucto Gaming.
                 </div>
-                <button className="w-[95%] bg-white text-[#050F40] py-3 rounded-3xl font-bold text-[18px]" onClick={()=>{return navigate("/signup")}}>
+                <button 
+                    className="w-full max-w-[95%] bg-white text-[#050F40] py-3 rounded-3xl font-bold text-[4vw] md:text-[18px]" 
+                    onClick={() => navigate("/signup")}
+                >
                     Get Started
                 </button>
             </div>
         </div>
-    )
+    );
 }
 
 export default Splash;
